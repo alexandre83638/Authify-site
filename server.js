6df2d0245919b93c91fcdee3c638c213c4e63550
Alexandre <alexandre@gmail.com>
2025-12-23 15:41:36 -0300

    primeiro deploy

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 console.log("INICIANDO SERVER...");
-
+    
+const path = require('path');
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static("public"));
 app.use(express.json());
 
@@ -194,6 +196,7 @@ app.post("/reset-key/:id", async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log("Servidor rodando na porta", PORT);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log('Servidor rodando na porta', PORT);
 });
+
